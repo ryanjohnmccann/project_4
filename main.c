5595f8e6918b79ffcc176b6e27b1c841f7bc9edc
Ryan McCann <ryanjohnmccann@gmail.com>
2022-05-03 09:55:44 -0400

    Read initial input file

--- main.c
+++ main.c
@@ -1,11 +1,42 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+
+char *input_file_name, *output_file_name;
+int random_seed;
+int mm_size, page_size, num_processes;
 
 void handle_infile(const char *file_name) {
+    FILE *fp;
+    int line_count = 0;
+    char line[50];
 
+    fp = fopen(file_name, "r");
+    while (fgets(line, sizeof(line), fp)) {
+        if (line_count == 0) {
+            mm_size = atoi(line);
+        }
+        else if (line_count == 1) {
+            page_size = atoi(line);
+        }
+        else if (line_count == 2) {
+            num_processes = atoi(line);
+        }
+        // TODO: Do I really need to read the other file names?
+        line_count += 1;
+    }
+    fclose(fp);
 }
+
 
 // Controls the program
 int main(int argc, char* argv[]) {
-    printf("Hello, World!\n");
+
+    input_file_name = argv[1];
+    output_file_name = argv[2];
+    random_seed = atoi(argv[3]);
+
+    handle_infile(input_file_name);
+
     return 0;
 }
--- mem_management.h
+++ mem_management.h
@@ -1,8 +1,11 @@
-//
-// Created by ryan on 5/1/22.
-//
 
 #ifndef PROJECT_4_MEM_MANAGEMENT_H
 #define PROJECT_4_MEM_MANAGEMENT_H
 
-#endif //PROJECT_4_MEM_MANAGEMENT_H
+// A single page table entry
+struct PTE {
+    // Status bits
+    int valid, reference, dirty;
+};
+
+#endif
